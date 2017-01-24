8369f93af24071238d9ebd4e10ec31817615cc46
Raul Marty <santor3medio@gmail.com>
2017-01-24 14:10:12 -0600

    Change background color to image in Una solucion educativa. Add another button, similar to orange but pink

--- src/app/main.js
+++ src/app/main.js
@@ -80,6 +80,7 @@ export class Main extends Component {
             <div className="prueba">
               <img src="assets/potencia.png" className="potencia-image" />
             </div>
+            <a className="btn-know" href="#" onClick={this.handleClick}>Conoce nuestra plataforma</a>
           </div>
           <video id="video" className="video" onPause={this.handleOnEnded}>
           </video>
