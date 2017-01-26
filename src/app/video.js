177a3090336395df982ed262ccbe51afc0456beb
Arturo Andrés Cidel Ortega <andres@cidel.com.mx>
2017-01-25 18:16:00 -0600

    Add comming-soon image to video path

--- src/app/video.js
+++ src/app/video.js
@@ -10,7 +10,7 @@ export class Video extends Component {
         <Header />
         <div className="potencia-video background">
           <div className="video-style">
-            <iframe width="660" height="415" src="https://www.youtube.com/embed/s1QQUbVipfo" frameBorder="0" allowFullScreen></iframe>
+            <div className="iframe"></div>
             <div className="text-center">
               <button className="btn btn-see">Ver video</button>
               <button className="btn btn-visit">Agendar una visita</button>
