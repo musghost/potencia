7f5dbe470bac14b040bab1c366bf682b50895ef3
alegabo93 <alegabo93@hotmail.com>
2017-05-03 11:00:19 -0500

    [comp] Add google button to login modal and connect it with api link

--- src/app/modals/login.js
+++ src/app/modals/login.js
@@ -1,44 +1,40 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import {Modal, ModalBody, Button, ModalHeader, ModalFooter} from 'elemental';
-import config from './../config';
+import React, {Component, PropTypes} from 'react';
+import {Modal, ModalBody, ModalHeader} from 'elemental';
 
 export class Login extends Component {
-
   constructor(props) {
     super(props);
-    this.closeModal = this.closeModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
-  closeModal() {
-    this.props.handleClick()
+  handleCloseModal() {
+    this.props.handleClick();
   }
 
-  render(){
+  render() {
+    const googleUrl = "http://potencia.pythonballz.com:80/api/v1/auth/google";
 
-    return(
-
+    return (
       <Modal isOpen={this.props.isOpen} onCancel={this.props.handleClick} backdropClosesModal width={'small'}>
-        <ModalHeader text="" showCloseButton onClose={this.props.handleClick} />
+        <ModalHeader text="" showCloseButton onClose={this.props.handleClick}/>
         <ModalBody>
           <form>
             <div className="message">
               <h2 className="title">Login</h2>
-              <p>Ingresa a Potencia y conoce todos los medios<br /> para tu clase.</p>
+              <p>Ingresa a Potencia y conoce todos los medios<br/> para tu clase.</p>
             </div>
             <div className="field-group">
-              <input type="email" ref="name" placeholder="Escribe tu correo"  />
+              <input type="email" ref="name" placeholder="Escribe tu correo"/>
               <span className="error-msg">El correo es requerido.</span>
             </div>
             <div className="field-group">
-              <input type="password" ref="email" placeholder="Escribe tu contraseña"  />
+              <input type="password" ref="email" placeholder="Escribe tu contraseña"/>
               <span className="error-msg">La contraseña es requerida.</span>
             </div>
           </form>
-          <div>
-            <p>
-              <button className="primary" type="button" onClick={this.closeModal}>Enviar</button>
-            </p>  
+          <div className="button-bar">
+            <button className="primary" type="button" onClick={this.handleCloseModal}>Enviar</button>
+            <a href={googleUrl} className="googleButton"><i className="fa fa-google-plus"></i>{' '} Inicia sesión</a>
           </div>
         </ModalBody>
       </Modal>
@@ -46,3 +42,8 @@ export class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  handleClick: PropTypes.func,
+  isOpen: PropTypes.bool
+};
